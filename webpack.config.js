fde5395349cd0a1c81516f6f0015487ff410b9e5
taniguchi <bayakau.tka@gmail.com>
2021-08-31 19:03:53 +0900

    feat ファイルローダーで画像をバンドルする場合

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  devtool: 'eval',
+  // devtool: 'eval',
   entry: ['./src/app.js', './src/main.js'],
   /* dist配下の出力ファイルを二つに分けたい時 */
   // entry: {
@@ -30,6 +30,23 @@ module.exports = {
           'css-loader',
           'postcss-loader',
           'sass-loader'
+        ]
+      },
+      // 画像ファイル用
+      {
+        test: /\.(jpe?g|png|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              // name: '[name].[ext]',
+              name: '[contenthash].[ext]',
+              // public/images配下に画像が格納される
+              outputPath: 'images',
+              // 画像をCDNに配置している場合などはpublicPathにそのURLを指定する
+              publicPath: 'http://127.0.0.1:5500/images'
+            }
+          }
         ]
       }
     ]
