3aea2facab06bf1db1f5f4f017b28252efcee6de
taniguchi <bayakau.tka@gmail.com>
2021-09-01 18:59:05 +0900

    feat 開発サーバー（webpack-dev-server）を追加

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {merge} = require('webpack-merge')
 const commonConf = require('./webpack.common')
@@ -6,6 +7,17 @@ const assetFile = '[name]'
 
 module.exports = () => merge(commonConf({outputFile, assetFile}), {
   mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+      watch: {
+        // 変更があっても無視する
+        ignored: /node_modules/ 
+      }
+    },
+    // devサーバーのURLを自動でブラウザで開いてくれる
+    open: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
